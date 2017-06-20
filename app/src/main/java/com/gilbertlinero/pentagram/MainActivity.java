61d07e422d28a1eaa3d9d464841dbae8fa69f828
gilberth <gilberth@redtic.org>
2017-06-19 21:16:06 -0500

    Parte 1 de Mascotas, Recycler View y Action View

--- app/src/main/java/com/gilbertlinero/pentagram/MainActivity.java
+++ app/src/main/java/com/gilbertlinero/pentagram/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
 
         agregarFAB();
 
